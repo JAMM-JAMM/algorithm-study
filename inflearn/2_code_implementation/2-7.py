a039e5c7e2c40a367d721d99d6c8d7baeacdd408
ShimJaeMin <apvmf32@gmail.com>
2023-01-22 01:37:49 +0900

    chore: 2-7

--- inflearn/2_code_implementation/2-7.py
+++ inflearn/2_code_implementation/2-7.py
@@ -10,5 +10,13 @@ import sys
 sys.stdin = open("input.txt")
 n = int(input())
 
-cnt = n - 1
+ch = [0] * (n+1)
+cnt = 0
+
 for i in range(2, n+1):
+    if ch[i] == 0:
+        cnt += 1
+        for j in range(i, n+1, i):
+            ch[j] = 1
+
+print(cnt)
