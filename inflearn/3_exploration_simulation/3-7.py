059c209025f5ec4468ff08af5018d4f57cec8e11
ShimJaeMin <apvmf32@gmail.com>
2023-02-04 09:56:19 +0900

    chore: 3-7 재풀이

--- inflearn/3_exploration_simulation/3-7.py
+++ inflearn/3_exploration_simulation/3-7.py
@@ -1,8 +1,36 @@
 '''
 사과나무(다이아몬드)
+'''
 
+'''
 풀이 날짜 : 2023-02-01
 풀이 소요 시간 : 풀이 참조
+'''
+
+# import sys
+
+
+# sys.stdin = open("input.txt", "r")
+# n = int(input())
+# n_mat = [list(map(int, input().split())) for _ in range(n)]
+
+# s = e = n // 2
+
+# res = 0
+
+# for i in range(n):
+#     for j in range(s, e+1):
+#         res += n_mat[i][j]
+#     if i < n // 2:
+#         s -= 1; e += 1
+#     else:
+#         s += 1; e -= 1
+
+# print(res)
+
+'''
+풀이 날짜 : 2023-02-04
+풀이 시간 : 8분
 '''
 
 import sys
@@ -12,16 +40,16 @@ sys.stdin = open("input.txt", "r")
 n = int(input())
 n_mat = [list(map(int, input().split())) for _ in range(n)]
 
-s = e = n // 2
+total_apple = 0
 
-res = 0
+s = e = n // 2
 
 for i in range(n):
     for j in range(s, e+1):
-        res += n_mat[i][j]
+        total_apple += n_mat[i][j]
     if i < n // 2:
         s -= 1; e += 1
     else:
         s += 1; e -= 1
 
-print(res)
+print(total_apple)
