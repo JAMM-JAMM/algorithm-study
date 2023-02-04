14c8fd4be116387c7b248d05b66d65228e7fce6c
ShimJaeMin <apvmf32@gmail.com>
2023-02-04 16:11:41 +0900

    chore: 4-1

---
+++ inflearn/4_binary_search_greedy/4-1.py
@@ -0,0 +1,29 @@
+'''
+이분 검색
+'''
+
+'''
+풀이 날짜 : 2023-02-04
+풀이 시간 : 풀이 참조
+'''
+
+import sys
+
+
+sys.stdin = open("input.txt", "r")
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+
+lst.sort()
+
+lt = 0; rt = n-1
+
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if lst[mid] == m:
+        print(mid + 1)
+        break
+    elif lst[mid] > m:
+        rt = mid - 1
+    else:
+        lt = mid + 1
