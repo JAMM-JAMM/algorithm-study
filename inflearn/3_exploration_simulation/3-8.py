6606211a4b79d84bb8ba560c87e3dd605f1a0664
ShimJaeMin <apvmf32@gmail.com>
2023-02-04 10:28:16 +0900

    chore: 3-8 재풀이

--- inflearn/3_exploration_simulation/3-8.py
+++ inflearn/3_exploration_simulation/3-8.py
@@ -1,8 +1,46 @@
 '''
 곳감(모래시계)
+'''
 
+'''
 풀이 날짜 : 2023-02-01
 풀이 소요 시간 : 풀이 참조
+'''
+
+# import sys
+
+
+# sys.stdin = open("input.txt", "r")
+# n = int(input())
+# n_mat = [list(map(int, input().split())) for _ in range(n)]
+
+# m = int(input())
+
+# for i in range(m):
+#     r, d, k = map(int, input().split())
+#     if d == 0: # 왼쪽으로 이동
+#         for _ in range(k):
+#             n_mat[r-1].append(n_mat[r-1].pop(0))
+#     else: # 오른쪽으로 이동
+#         for _ in range(k):
+#             n_mat[r-1].insert(0, n_mat[r-1].pop())
+
+# res = 0
+# s = 0; e = n-1
+
+# for i in range(n):
+#     for j in range(s, e+1):
+#         res += n_mat[i][j]
+#     if i < n // 2:
+#         s += 1; e -= 1
+#     else:
+#         s -= 1; e += 1
+
+# print(res)
+
+'''
+풀이 날짜: 2023-02-04
+풀이 시간: 
 '''
 
 import sys
@@ -11,27 +49,27 @@ import sys
 sys.stdin = open("input.txt", "r")
 n = int(input())
 n_mat = [list(map(int, input().split())) for _ in range(n)]
-
 m = int(input())
 
 for i in range(m):
     r, d, k = map(int, input().split())
-    if d == 0: # 왼쪽으로 이동
+    if d == 0:
         for _ in range(k):
             n_mat[r-1].append(n_mat[r-1].pop(0))
-    else: # 오른쪽으로 이동
+    else:
         for _ in range(k):
             n_mat[r-1].insert(0, n_mat[r-1].pop())
 
-res = 0
+tot = 0
+
 s = 0; e = n-1
 
 for i in range(n):
     for j in range(s, e+1):
-        res += n_mat[i][j]
+        tot += n_mat[i][j]
     if i < n // 2:
         s += 1; e -= 1
     else:
         s -= 1; e += 1
 
-print(res)
+print(tot)
