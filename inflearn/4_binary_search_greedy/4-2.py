a2da72a0e6922e57dea2027941c0e117d8348b19
ShimJaeMin <apvmf32@gmail.com>
2023-02-07 17:13:20 +0900

    chore: 4-2 재풀이

--- inflearn/4_binary_search_greedy/4-2.py
+++ inflearn/4_binary_search_greedy/4-2.py
@@ -6,36 +6,74 @@
 풀이날짜 : 2023-02-04
 '''
 
+# import sys
+
+
+# sys.stdin = open("input.txt", "r")
+# k, n = map(int, input().split())
+
+# largest = 0
+# lst = []
+# for _ in range(k):
+#     tmp = int(input())
+#     lst.append(tmp)
+#     largest = max(tmp, largest)
+
+# def Count(len):
+#     cnt = 0
+#     for x in lst:
+#         cnt += (x // len)
+#     return cnt
+
+# lt = 1
+# rt = largest
+# res = 0
+# while lt <= rt:
+#     mid = (lt + rt) // 2
+#     if Count(mid) >= n:
+#         res = mid
+#         lt = mid + 1
+#     else:
+#         rt = mid - 1
+#     # print("lt = {}, rt = {}, mid = {}, res = {}, count = {}".format(lt, rt, mid, res, Count(mid)))
+
+
+# print(res)
+
+'''
+풀이 날짜 : 2023-02-07
+'''
+
 import sys
 
 
 sys.stdin = open("input.txt", "r")
 k, n = map(int, input().split())
 
+lst = list()
 largest = 0
-lst = []
+
 for _ in range(k):
     tmp = int(input())
     lst.append(tmp)
-    largest = max(tmp, largest)
+    largest = max(largest, tmp)
 
-def Count(len):
+lt = 1 # 랜선을 만들 수 있는 최소 구간의 초기 값
+rt = largest # 랜선을 만들 수 있는 최대 구간의 초기 값
+res = 0 # N개를 만들 수 있는 랜선의 최대 길이
+
+def Counter(length):
     cnt = 0
     for x in lst:
-        cnt += (x // len)
+        cnt += (x // length)
     return cnt
 
-lt = 1
-rt = largest
-res = 0
 while lt <= rt:
     mid = (lt + rt) // 2
-    if Count(mid) >= n:
-        res = mid
+    if Counter(mid) >= n:
         lt = mid + 1
+        res = mid
     else:
         rt = mid - 1
-    # print("lt = {}, rt = {}, mid = {}, res = {}, count = {}".format(lt, rt, mid, res, Count(mid)))
-
 
 print(res)
