ec4aa1b044d294045d9d279123612ecad33a68db
ShimJaeMin <apvmf32@gmail.com>
2023-02-07 17:27:16 +0900

    chore: 4-1 재풀이

--- inflearn/4_binary_search_greedy/4-1.py
+++ inflearn/4_binary_search_greedy/4-1.py
@@ -7,23 +7,50 @@
 풀이 시간 : 풀이 참조
 '''
 
+# import sys
+
+
+# sys.stdin = open("input.txt", "r")
+# n, m = map(int, input().split())
+# lst = list(map(int, input().split()))
+
+# lst.sort()
+
+# lt = 0; rt = n-1
+
+# while lt <= rt:
+#     mid = (lt + rt) // 2
+#     if lst[mid] == m:
+#         print(mid + 1)
+#         break
+#     elif lst[mid] > m:
+#         rt = mid - 1
+#     else:
+#         lt = mid + 1
+
+'''
+풀이 날짜 : 2023-02-07
+풀이 시간 : 풀이 참조
+'''
+
 import sys
 
 
 sys.stdin = open("input.txt", "r")
 n, m = map(int, input().split())
-lst = list(map(int, input().split()))
+n_lst = list(map(int, input().split()))
 
-lst.sort()
+n_lst.sort()
 
-lt = 0; rt = n-1
+lt = 0
+rt = n - 1
 
 while lt <= rt:
     mid = (lt + rt) // 2
-    if lst[mid] == m:
-        print(mid + 1)
+    if n_lst[mid] == m:
+        print(mid+1)
         break
-    elif lst[mid] > m:
+    elif n_lst[mid] > m:
         rt = mid - 1
     else:
         lt = mid + 1
